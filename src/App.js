290b75b93130d08635dd4aa57e6586311ecf0fdc
kigra58 <kigra58@outlook.com>
2021-09-12 20:31:12 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import './bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+     <div class="container p-5">
+
+<table class="table display" id="myTable" cellspacing="0" width="100%">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>first_name1</td>
+        <td>last_name1</td>
+        <td>mail1.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>first_name2</td>
+        <td>last_name2</td>
+        <td>mail2.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>first_name3</td>
+        <td>last_name3</td>
+        <td>mail3.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">4</th>
+        <td>first_name4</td>
+        <td>last_name4</td>
+        <td>mail4.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">5</th>
+        <td>first_name5</td>
+        <td>last_name5</td>
+        <td>mail5.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">6</th>
+        <td>first_name6</td>
+        <td>last_name6</td>
+        <td>mail6.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">7</th>
+        <td>first_name7</td>
+        <td>last_name7</td>
+        <td>mail7.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">8</th>
+        <td>first_name8</td>
+        <td>last_name8</td>
+        <td>mail8.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">9</th>
+        <td>first_name9</td>
+        <td>last_name9</td>
+        <td>mail9.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">10</th>
+        <td>first_name10</td>
+        <td>last_name10</td>
+        <td>mail10.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">11</th>
+        <td>first_name11</td>
+        <td>last_name11</td>
+        <td>mail11.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">12</th>
+        <td>first_name12</td>
+        <td>last_name12</td>
+        <td>mail12.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">13</th>
+        <td>first_name13</td>
+        <td>last_name13</td>
+        <td>mail13.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">14</th>
+        <td>first_name14</td>
+        <td>last_name14</td>
+        <td>mail14.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">15</th>
+        <td>first_name15</td>
+        <td>last_name15</td>
+        <td>mail15.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">16</th>
+        <td>first_name16</td>
+        <td>last_name16</td>
+        <td>mail16.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">17</th>
+        <td>first_name17</td>
+        <td>last_name17</td>
+        <td>mail17.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">18</th>
+        <td>first_name18</td>
+        <td>last_name18</td>
+        <td>mail18.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">19</th>
+        <td>first_name19</td>
+        <td>last_name19</td>
+        <td>mail19.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">20</th>
+        <td>first_name20</td>
+        <td>last_name20</td>
+        <td>mail20.@mail.com</td>
+      </tr>
+      <tr>
+        <th scope="row">21</th>
+        <td>first_name21</td>
+        <td>last_name21</td>
+        <td>mail21.@mail.com</td>
+      </tr>
+      
+    </tbody>
+  </table>
+
+</div>
+
+
     </div>
   );
 }
